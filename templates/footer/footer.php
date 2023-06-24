cb6196e6e620a941e9b003e6e92f42d3e0d6477f
vothanhdanh <999conan999@gmail.com>
2023-06-24 18:57:04 +0700

    Asdae11111

--- templates/footer/footer.php
+++ templates/footer/footer.php
@@ -52,12 +52,12 @@ $footer='<footer id="footerz-dt">';
         $footer.='<ul class="ulz">';
              $footer.='<li><a id="cuoc-goi-2" href="tel:'.$common->lien_he_sdt.'" rel="nofollow" onclick="window.value=75000"><i class="icon-call calzs"></i></a><span class="aml-text-content aml-tooltiptext">Gọi ngay: '.$formattedPhoneNumber.'</span></li>';
              $footer.='<li><a id="lien-he-zalo-1" target="_blank" href="https://zalo.me/'.$common->lien_he_zalo.'" rel="nofollow" onclick="window.value=90000" class="re"><span class="icon-zalo path1"><span class="path1"></span><span class="path2"></span></span></a><span class="aml-text-content aml-tooltiptext">Chat với chúng tôi qua Zalo</span></li>';
-             $footer.='<li><a id="lien-he-fb" href="'.$common->lien_he_fb.'" rel="nofollow" onclick="window.value=60000"><span class="icon-mess icon-messenger"></span></a><span class="aml-text-content aml-tooltiptext">Nhắn tin với chúng tôi qua facebook</span></li>';
+             $footer.='<li><a id="lien-he-fb" target="_blank" href="'.$common->lien_he_fb.'" rel="nofollow" onclick="window.value=60000"><span class="icon-mess icon-messenger"></span></a><span class="aml-text-content aml-tooltiptext">Nhắn tin với chúng tôi qua facebook</span></li>';
              $footer.='<li onclick="show_cart()"><a id="my-cart" onclick="window.value=60000"><span class="icon-cartx icon-messenger re"><b class="notify" id="cart-2">0</b></span></a><span class="aml-text-content aml-tooltiptext">Giỏ hàng</span></li>';
         $footer.='</ul>';
     $footer.='</div>';
     if($type=="post"){
-    $footer.='<div id="scrollTop" class="contactsx"><div class="rap-contanct">👉<a href="'.$post_infor->cate->url.'" class="contacktsx re" target="_blank" title="'.$post_infor->key_word.'">Xem thêm: '.$post_infor->key_word.'<span class="anime">1</span></a></div></div>';
+    $footer.='<div id="scrollTop" class="contactsx"><div class="rap-contanct">👉<a href="'.$post_infor->cate->url.'" class="contacktsx re" title="'.$post_infor->key_word.'">Xem thêm: '.$post_infor->key_word.'<span class="anime">1</span></a></div></div>';
     }
     $footer.='<div id="popup-cart" class="popup-cart"></div>';
     $footer.='<div id="check-kt" class="check-kt" onclick="hiden_check_kt()"></div>';

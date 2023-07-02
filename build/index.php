1a98eb3247c73103e16748c60f73893e42eba386
vothanhdanh <999conan999@gmail.com>
2023-07-02 16:57:49 +0700

    xxxxcccccccccasedqweqw1

--- build/index.php
+++ build/index.php
@@ -1,1 +1,16 @@
-admin
+ 
+<?php $home=get_home_url(); ?>
+<head>
+    <script type="text/javascript">window.url = "<?php echo $home ?>"</script>
+    <link async rel="stylesheet" href="<?php echo $home; ?>/wp-content/themes/shopseo/build/fontawesome/css/all.min.css" />
+    <script defer="defer" src="<?php echo $home;?>/wp-content/themes/shopseo/build/tinymce/tinymce.min.js"></script>
+
+    <script defer="defer" src="<?php echo $home; ?>/wp-content/themes/shopseo/build/static/js/main.0fb1ac4f.js"></script>
+    <link href="<?php echo $home; ?>/wp-content/themes/shopseo/build/static/css/main.042105d9.css" rel="stylesheet">
+
+
+</head>
+
+<div  style=" position: fixed; top: 0px; left: 0px; width: 100%; z-index: 99999; background-color: #f9f9f9; ">
+    <div id="root" style=" height: 100vh; overflow: scroll; "></div>
+</div>
--- taxonomy.php
+++ taxonomy.php
@@ -1,5 +1,5 @@
 <?php 
     $home_url=get_home_url();
-    header("Location: ".$home_url);
+    header("Location: ".$home_url,TRUE,301);
     die();
 ?>

784b8225834ce375bd2902cc724773924aef8420
vothanhdanh <999conan999@gmail.com>
2023-07-30 01:15:09 +0700

    OKV20

--- templates/ajax/posts/get_infor_post.php
+++ templates/ajax/posts/get_infor_post.php
@@ -6,12 +6,13 @@ require_once( $parse_uri[0] . 'wp-load.php' );	// global $wpdb;
 function get_post_infor($id){// search phone or email ~~ '' => get all
    global $wpdb;
    $table_prefix=$wpdb->prefix .'shopseo_posts';
-        $sql = $wpdb->prepare( "SELECT quantity_sold,related_keyword,post_status,is_best_seller,json_data FROM $table_prefix WHERE id_post = %d",$id);
+        $sql = $wpdb->prepare( "SELECT quantity_sold,thumnail,related_keyword,post_status,is_best_seller,json_data FROM $table_prefix WHERE id_post = %d",$id);
    $results = $wpdb->get_results( $sql , OBJECT );
 
    $json_data=json_decode($results[0]->json_data); 
    $json_data->id=$id;
    $json_data->quantity_sold=$results[0]->quantity_sold;
+   $json_data->thumnail=json_decode($results[0]->thumnail);
    $json_data->status=$results[0]->post_status;
    $json_data->is_best_seller=$results[0]->is_best_seller;
    $json_data->related_keyword=json_decode($results[0]->related_keyword);

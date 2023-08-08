fcae3fec21361070523501016324c3994a323c17
vothanhdanh <999conan999@gmail.com>
2023-08-08 21:38:00 +0700

    ok v26

--- templates/fs.php
+++ templates/fs.php
@@ -51,7 +51,7 @@ function render_xml_first(){
     $Path_name_xml=ABSPATH.'wp-content/themes/shopseo/templates/ajax/xml/products.xml';
     global $wpdb;
     $table_prefix=$wpdb->prefix .'shopseo_posts';
-    $sql = $wpdb->prepare( "SELECT id_post,title,short_des,price,thumnail,shoping_type,instock,id_category FROM $table_prefix WHERE shoping_on_off = 'on' AND post_status = 'publish' ORDER BY id_category DESC ");
+    $sql = $wpdb->prepare( "SELECT id_post,title,short_des,price,thumnail,img_shoping,shoping_type,instock,id_category FROM $table_prefix WHERE shoping_on_off = 'on' AND post_status = 'publish' ORDER BY id_category DESC ");
     $results = $wpdb->get_results( $sql , OBJECT );
     if(count($results)>0){
       $home_url=get_home_url();
@@ -73,8 +73,13 @@ function render_xml_first(){
           $values['brand'] = $name_brand;
           $values['gtin'] = "";
           $values['mpn'] = "";
-              $thumnail=json_decode($x->thumnail);
-          $values['image_link'] = $thumnail->url;
+        if($x->img_shoping==""||$x->img_shoping==" "||$x->img_shoping==NULL){
+            $thumnail=json_decode($x->thumnail);
+        $values['image_link'] = $thumnail->url;
+        }else{
+        $values['image_link'] = $x->img_shoping;
+        }
+        //
           $values['google_product_category'] = "";
           $values['custom_label_0'] = $x->shoping_type;
           array_push($rs,$values);

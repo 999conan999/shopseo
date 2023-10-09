db18a814641304fdf9e8527f6306bc8abfe0880e
vothanhdanh <999conan999@gmail.com>
2023-10-09 22:08:04 +0700

    ok titosks v1

--- templates/ajax/media/upload_core.php
+++ templates/ajax/media/upload_core.php
@@ -1,8 +1,6 @@
 <?php 
     $parse_uri = explode( 'wp-content', $_SERVER['SCRIPT_FILENAME'] );
     require_once( $parse_uri[0] . 'wp-load.php' );
-    //**** */ kiểm tra login ở đây
-    // var_dump(is_user_logged_in());
 
     if ( ! function_exists( 'wp_handle_upload' ) ) {
         require_once( ABSPATH . 'wp-admin/includes/file.php' );

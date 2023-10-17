5cbb6a13496598c968023a1981d6fd441035b0cb
vothanhdanh <999conan999@gmail.com>
2023-10-17 14:42:03 +0700

    ok final video rm v11

--- templates/archive/main.php
+++ templates/archive/main.php
@@ -91,7 +91,7 @@
                             <li class="splide__slide">
                                 <div class="item-video">
                                     <?php if (strpos($video->url, '.mp4') !== false) { ?>
-                                    <video playsinline type="video/mp4" class="var-video cs-video" width="100%" <?php echo $poster==""?'':'poster="'.$poster.'"'; ?> <?php echo $i==0?'src="'.$video->url.'"':'data-src="'.$video->url.'"'; ?> onclick="fs_play_index(<?php echo $i; ?>)"></video>
+                                    <video playsinline type="video/mp4" class="var-video cs-video" width="100%" <?php echo $poster==""?'':'poster="'.$poster.'"'; ?> <?php echo $i==0?'src="'.$video->url.'" '.'data-src="'.$video->url.'"':'data-src="'.$video->url.'"'; ?> onclick="fs_play_index(<?php echo $i; ?>)"></video>
                                     <div class="centered-image" id="play-<?php echo $i; ?>"  onclick="fs_play_index(<?php echo $i; ?>)">
                                         <img src="<?php echo $home_url;?>/wp-content/themes/shopseo/templates/src/play.png" class="play-icon">
                                     </div>

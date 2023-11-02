39634d76c888f153cc2351fca2ce775cd59a217c
vothanhdanh <999conan999@gmail.com>
2023-11-02 13:06:29 +0700

    ok ridirect

--- templates/404/control.php
+++ templates/404/control.php
@@ -3,6 +3,17 @@
     $home_url=get_home_url();
     $home_name=str_replace('https://', '', $home_url);
     $home_name=str_replace('http://', '', $home_name);
+    //
+    $redirects = array(
+        '/cofanew/xxxxx' => '/tu-sat',
+        // Thêm nhiều cặp slug và URL khác nếu cần
+    );
+    // Kiểm tra URL slug và chuyển hướng nếu cần
+    if (isset($redirects[$_SERVER['REQUEST_URI']])) {
+        header("Location: " . $home_url . $redirects[$_SERVER['REQUEST_URI']], TRUE, 301);
+        die();
+    }
+    //
     if($common->ridirect_404_301_home){
         header("Location: ".$home_url,TRUE,301);
         die();
